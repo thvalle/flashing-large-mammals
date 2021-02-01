65b1c1664990bb168afe3ddbb7f640faeff3d0b7
Torgeir <torgeirholval@gmail.com>
2021-02-01 18:47:54 +0100

    Oppdatert surv_in_process før mail til veiledere 1. feb

--- Skript/Data_exploration2.R
+++ Skript/Data_exploration2.R
@@ -5,7 +5,7 @@ library(plyr)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
-
+setwd()
 # Import data
 effort<-readRDS("Effort_prepared.rds")
 obs<-readRDS("Observations_prepared1.rds")
@@ -29,7 +29,7 @@ for(i in 2:length(unique(obs$validated_species))){
 }
 
 effort.stat$validated_species<-rep(as.character(unique(obs$validated_species)), each=nrow(temp)) # assigning the validated_species 
-View(effort.stat) # Looks good
+# View(effort.stat) # Looks good
 
 freq<-merge(obs.stat, effort.stat, by=c("loc", "period", "validated_species"), all=TRUE)
 # Merging issue here should disapper after you have had a look at the gaps.
@@ -142,37 +142,42 @@ time.dep<-merge(time.dep, obs.agg, by=c("loc","date","validated_species"), all.x
 
 time.dep[is.na(time.dep$n.obs),"n.obs"]<-0
 
-# Plotting average number of events against time since deploy
-sp="rev"
-sp.freq<-ddply(time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control",], .(flash, time.deploy), summarise,
-      mean.p=mean(n.obs))
+# Plotting average number of events against time since deploy -------------------
+sp="raadyr"
+sp.freq<-ddply(time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control",], 
+               .(flash, time.deploy), summarise,
+                mean.p=mean(n.obs))
 
 plot(mean.p ~ time.deploy, sp.freq[sp.freq$flash %in% 0,], type="l")
 lines(mean.p ~ time.deploy, sp.freq[sp.freq$flash %in% 1,], col="red")
 
-#                                      sp.freq <- sp.freq[!sp.freq$mean.p>.4,] # remove impossible frequency values
-#                                      sp.freq[sp.freq$mean.p>.4,] # observations removed
-# TODO                            In addition there are some NA's in the $loc that I do not understand  
 
-# Generalized linear model to test if there is an effect of time since deployment (i.e. are the visitation rate declining with flash)
+# Generalized linear model to test if there is an effect of time since deployment 
+# (i.e. are the visitation rate declining with flash)
 time.dep$loc<-as.factor(time.dep$loc)
-my.glm<-glm(n.obs~time.deploy+as.factor(flash), time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control",], family = poisson)
+my.glm <- glm(n.obs ~ time.deploy + as.factor(flash), 
+            time.dep[time.dep$validated_species %in% sp & !time.dep$period %in% "Control",], family = poisson)
 summary(my.glm) # Ignores the fact that there might be seasonal effects or potential effect of camera site. 
 
-new.dat<-expand.grid(flash=c(0,1), time.deploy=0:100)
-new.dat$fit<-predict(my.glm, newdata=new.dat, type="response")
+new.dat <- expand.grid(flash = c(0,1), time.deploy = 0:100)
+new.dat$fit <- predict(my.glm, newdata = new.dat, type = "response")
 
-plot(fit~time.deploy, new.dat[new.dat$flash%in%0,], ylim=c(0,0.2))
-lines(fit~time.deploy, new.dat[new.dat$flash%in%1,])
+plot(fit ~ time.deploy, new.dat[new.dat$flash %in% 0,], ylim=c(0,0.2), type = "l",
+     ylab = "Events per day", xlab = "Days after camera operation")
+lines(fit ~ time.deploy, new.dat[new.dat$flash %in% 1,], col="red")
+legend("bottomleft", legend = c("white LED flash", "IR flash"), col = c("red","black"), lty = c(1, 1))
+
 
 library(lme4)
 # Testing with generalized linear mixed models
-time.dep$month<-as.factor(format(time.dep$date, "%m"))
-time.dep$loc<-as.factor(time.dep$loc)
-time.dep$time.deploy<-time.dep$time.deploy/10
+time.dep$month <- as.factor(format(time.dep$date, "%m")) # retrieve months of the date column
+time.dep$loc <- as.factor(time.dep$loc)                 # make loc into factor
+time.dep$time.deploy <- time.dep$time.deploy / 10       # make time.deploy more concentrated (1/10 in size)
 
 # Model with random effect of loc and month on the intercept. 
-my.glmer<-glmer(n.obs~time.deploy+as.factor(flash)+(1|loc)+(1|month), time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control"& time.dep$time.deploy<6.1,], family = poisson)
+my.glmer<-glmer(n.obs~time.deploy+as.factor(flash)+(1|loc)+(1|month), 
+                time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control"& time.dep$time.deploy<6.1,],
+                family = poisson)
 summary(my.glmer)
 
 # Plotting the fixed effects of the model. I need to look up a way to do this correctly... 
@@ -180,6 +185,282 @@ new.dat<-expand.grid(flash=c(0,1), time.deploy=0:10)
 new.dat$fit<-exp(-3.44897+0.05823*new.dat$time.deploy+0.14379*new.dat$flash)
 new.dat$time.deploy<-new.dat$time.deploy*10 # Rescaling back to the original scale
 
-plot(fit~time.deploy, new.dat[new.dat$flash%in%0,], ylim=c(0,0.2), type="l")
+plot(fit~time.deploy, new.dat[new.dat$flash%in%0,], ylim=c(0,0.2), type="l",
+     ylab = "Events per day", xlab = "Days after camera operation")
 lines(fit~time.deploy, new.dat[new.dat$flash%in%1,], ylim=c(0,0.2), col="red")
+legend("bottomleft", legend = c("white LED flash", "IR flash"), col = c("red","black"), lty = c(1, 1))
+
+
+# tidymodels attempt - Torgeir -------------------------------------
+
+covs <- readRDS("CTloc_covs.rds") %>% 
+        #remove geometry, cause of object-type,
+        # lon, build_dens and field_d2 cause of correlations
+  select(!c(3,4,8,12))%>%
+  as.data.frame() %>% # Chaning class to data.fram and not a sf data.frame
+  mutate( LokalitetID = factor(LokalitetID))
+class(covs); names(covs)
+
+set.seed(123)
+flash_data <- 
+  time.dep %>% 
+  # Convert the loc and flash to factor, make a numeric date-col
+  # convert n.obs into factor with many breaks,
+  # because outcome=factor is required for fitting
+  mutate( loc = factor(loc),
+          flash = factor(flash), 
+          numeric_date = as.numeric(date),
+          month = as.factor(format(time.dep$date, "%m"))
+          #n.obs = factor(n.obs, levels = 0:15, ordered = T)
+          ) %>%
+  # Include the covs data
+  inner_join(covs, by = c("loc" = "LokalitetID")) %>% 
+  rename(species = validated_species) %>% 
+  # Exclude missing data
+  na.omit() %>% 
+  # For creating models, it is better to have qualitative columns
+  # encoded as factors (instead of character strings)
+  mutate_if(is.character, as.factor)
+names(flash_data)
+
+fjern <- flash_data %>% #making a vector to remove
+  group_by(species) %>% #all "species" with very few sightings
+  summarise(sum = sum(n.obs)) %>% 
+  filter(sum<40) %>% distinct(species)
+flash_data <- flash_data %>% # removing them, and "unknown",
+  filter(!species %in% fjern$species, # null, and nothing
+         !species %in% c("ukjent", "null", "nothing"))
+flash_data %>% 
+  distinct(species) # 17 "species" left
+unique(flash_data$n.obs)
+
+flash_data %>% 
+  skimr::skim(n.obs, species)
+library(tidyverse);library(tidymodels)
+library(modelr)
+# Fix the random numbers by setting the seed 
+# This enables the analysis to be reproducible when random numbers are used 
+{set.seed(555)
+# Put 3/4 of the data into the training set 
+data_split <- initial_split(flash_data, prop = 3/4)
+
+# Create data frames for the two sets:
+train_data <- training(data_split)
+test_data  <- testing(data_split)
+
+test_data %>% 
+  distinct(species) %>% 
+  anti_join(train_data) 
+#no species that are only in the test set.
+}
+
+by_sp <- flash_data %>% 
+  group_by(species) %>%
+# although I don't understand why it is important to remove control
+  filter(!period == "Control") %>% 
+  nest()
+by_sp
+# 
+# # don't understand (1|loc)+(1|month), and it doesn't work
+# sp_model <- function(df) {
+#   glm(n.obs ~ time.deploy + flash + loc + month,
+#       data = df)
+# }
+# # Model with random effect of loc and month on the intercept. 
+# my.glmer<-glmer(n.obs~time.deploy+as.factor(flash)+(1|loc)+(1|month), 
+#                 time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control"& time.dep$time.deploy<6.1,],
+#                 family = poisson)
+# summary(my.glmer)
+# 
+# 
+sp_model <- function(df) {
+  lm(n.obs ~ time.deploy + flash, #+ loc + month,
+      data = df)
+}
+
+models <- map(by_sp$data, sp_model)
+
+by_sp <- by_sp %>% 
+  mutate(model = map(data, sp_model))
+by_sp %>% 
+  arrange(species)
+# unnesting
+#   Obs!   There are no residuals in GLM !!
+by_sp <-  by_sp %>%
+  mutate(
+    resids = map2(data, model, add_residuals) #modelr needed!
+    )
+by_sp
+
+              # resids <- unnest(by_sp, resids)
+              # resids                            # Computer freezes!
+
+#resids %>% 
+  ggplot(aes(n.obs, resid)) +
+  geom_line(aes(group = species), alpha = 1 / 3) + 
+  geom_smooth(se = FALSE)
+
+
+  
+# Model with random effect of loc and month on the intercept. 
+my.glmer<-glmer(n.obs~time.deploy+as.factor(flash)+(1|loc)+(1|month), 
+                time.dep[time.dep$validated_species%in%sp & !time.dep$period%in%"Control"& time.dep$time.deploy<6.1,],
+                family = poisson)
+summary(my.glmer)
+
+# Plotting the fixed effects of the model. I need to look up a way to do this correctly... 
+new.dat <- expand.grid(flash = c(0,1), time.deploy = 0:10)
+new.dat$fit <- exp( - 3.44897 + 0.05823 * new.dat$time.deploy +
+                      0.14379 * new.dat$flash)
+# Rescaling back to the original scale
+new.dat$time.deploy <- new.dat$time.deploy * 10
+
+plot(fit~time.deploy, new.dat[new.dat$flash%in%0,],
+     ylim=c(0,0.2), type="l")
+lines(fit~time.deploy, new.dat[new.dat$flash%in%1,], 
+      ylim=c(0,0.2), col="red")
+
+
+# Infer --------------------------------------------------------
+library(tidymodels) # Includes the infer package
+sp=c("rev", "gaupe", "raadyr", "elg", "gaupe", "grevling")
+set.seed(313)
+sp_pval <- tibble(.rows = 2)
+  for (i in sp) {
+  flash_data_sp <- filter(flash_data, species %in% sp, !period %in% "Control")
+  d_hat <- flash_data_sp %>% 
+    specify(n.obs ~ flash) %>%
+    calculate(stat = "diff in means", order = c(1, 0))
+  # Then, generating the null distribution,
+  null_distn <- flash_data_sp %>%
+    specify(n.obs ~ flash) %>%
+    hypothesize(null = "independence") %>% 
+    generate(reps = 1000, type = "permute") %>% 
+    calculate(stat = "diff in means", order = c(1, 0))
+  # Visualizing the observed statistic alongside the null distribution,
+  visualise(null_distn) + labs(tag = sp) +
+    shade_p_value(obs_stat = d_hat, direction = "two-sided")
+  sp_pval[[i]] <- null_distn %>%
+    get_p_value(obs_stat = d_hat, direction = "two-sided")
+}
+sp_pval
+
+flash_data_sp <- filter(flash_data, species %in% sp, !period %in% "Control")
+d_hat <- flash_data_sp %>% 
+  specify(n.obs ~ flash) %>%
+  calculate(stat = "diff in means", order = c(1, 0))
+# Then, generating the null distribution,
+null_distn <- flash_data_sp %>%
+  specify(n.obs ~ flash) %>%
+  hypothesize(null = "independence") %>% 
+  generate(reps = 1000, type = "permute") %>% 
+  calculate(stat = "diff in means", order = c(1, 0))
+# Visualizing the observed statistic alongside the null distribution,
+visualise(null_distn) + labs(tag = sp) +
+  shade_p_value(obs_stat = d_hat, direction = "two-sided")
+null_distn %>%
+  get_p_value(obs_stat = d_hat, direction = "two-sided")
+# elg p-value   : 0.54
+# raadyr --||-- : 0.388
+# grevling -||- : 0.142
+# rev   --||--  : 0.05
+# hjort --||--  : 0.014
+# gaupe --||--  : 0 #please be cautious in reporting a p-value of 0
+
+help(package = "infer")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# didn't manage :( -------------------------------
+sp <- "raadyr"
+train_data_sp <- filter(train_data, species %in% sp)
+# Make recipe
+flash_rec <- 
+  recipe(n.obs ~ time.deploy + flash,
+         data = train_data_sp) %>% 
+  #extracting month and day of week as predictors
+  # step_date(date, features = c("dow", "month")) %>% 
+  # update_role(date, new_role = "ID") %>% # keeping date as an ID
+  step_normalize(all_numeric()) %>% 
+  # step_dummy(all_nominal(), -all_outcomes()) %>%  #making dummy variables
+  step_zv(all_predictors()) #removing any predictors containing only one value
+
+summary(flash_rec)
+# Make model
+lr_mod <- 
+  logistic_reg() %>% 
+  set_engine("glm")
+# Make workflow
+flash_wflow <- 
+  workflow() %>% 
+  add_model(lr_mod) %>% 
+  add_recipe(flash_rec)
+flash_wflow
+
+flash_fit <- 
+  flash_wflow %>% 
+  fit(data = train_data_sp)
+
+flash_fit %>% 
+  pull_workflow_fit() %>% 
+  tidy()
+
+test_data_sp <- filter(test_data, species %in% sp)
+predict(flash_fit, test_data_sp)
+
+flash_pred <- 
+  predict(flash_fit, test_data_sp, type = "prob") %>% 
+  bind_cols(test_data_sp %>% select(n.obs, time.deploy, flash, loc)) 
+
+# The data look like: 
+flash_pred
+# ROC curve
+flash_pred %>%
+  roc_curve(truth= n.obs, .pred_0) %>% autoplot()
+
+
+
+
+
+
+#step_geodist() 
+#step_
+# PLS part
+set.seed(57343)
+folds <- vfold_cv(flash_data, repeats = 10)
+
+folds <- 
+  folds %>%
+  mutate(recipes = map(splits, prepper, recipe = norm_rec))
+
+
+
+# Make model
+lr_mod <- 
+  logistic_reg() %>% 
+  set_engine("glm")
+# Make workflow
+flash_wflow <- 
+  workflow() %>% 
+  add_model(lr_mod) %>% 
+  add_recipe(flash_rec)
+flash_wflow
+
+flash_fit <- 
+  flash_wflow %>% 
+  fit(data = train_data)
+
+flash_fit %>% 
+  pull_workflow_fit() %>% 
+  tidy()
 
--- Skript/Time_to_tidymodel.R
+++ Skript/Time_to_tidymodel.R
@@ -4,103 +4,77 @@ library(tidymodels)
 library(survival)
 library(survminer)
 # read data from Time_to_event_Torgeir5.R
-sp = c("rev", "raadyr", "gaupe", "hjort", "grevling", "elg")
 covs<-readRDS("CTloc_covs.rds") %>% as.data.frame() 
 library(corrplot)
 covs %>% select(!c(1,3,4)) %>% cor() %>% 
   corrplot(type = "upper", method = "number")
 names(covs)
-obs <- readRDS("obs_tte5.rds") %>% filter(validated_species %in% sp) %>% 
+
+obs <- readRDS("obs_tte5.rds") %>% rename(species = validated_species) %>% 
   merge(covs, by.x="loc", by.y="LokalitetID", all.x=TRUE, all.y=FALSE) %>% 
   # removing various IDs, lat lon which correlates with slope, build dens,
   # field_d2, as well as geometry which I don't understand what is
-  select(-c(distance, num_animals, 4:7, 14, 19:21) ) %>%  
+  select(-c(4:9, 11, 19:21) ) %>% 
   # Exclude missing data
   na.omit() %>% 
   # For creating models, it is better to have qualitative columns
   # encoded as factors (instead of character strings)
   mutate_if(is.character, as.factor)
-
+names(obs)
 
 
 obs %>% 
-  skimr::skim(validated_species, flashed) 
-
-
-# mod0 from Neri
-mod0<-coxph(Surv(t.diff, event, type="right")~flashed, data=obs[obs$validated_species%in%sp & !obs$period%in%"Control",])
-summary(mod0)
-# Keep in mind that positive values means higher risk of "dying" or being detected again, and negative values means lower risk of "dying" or being detected again
-# In this case the positive sign for flashed means that when the animal was flashed the time to new detection is shorter than when the animal was not flashed. 
-# However, the effect is not significant. 
-
-
-?survreg
-# mod example from tidymodels
-lung_mod <- survreg(Surv(time, status) ~ ph.ecog + age + strata(sex), data = lung)
-summary(lung_mod)
-
-# explore for yourself
-sp_mod <- coxph(Surv(t.diff, event, type="right") ~ flashed + strata(validated_species),
-                data = obs[!obs$period%in%"Control",]) # remove control data
-sp_mod
-
-fit <- survfit(Surv(t.diff, event, type="right") ~ flashed + strata(validated_species), data = obs[!obs$period%in%"Control",]) 
-ggsurvplot(fit, data = obs[!obs$period%in%"Control",])
-# crazy amounts of species! Filter out the ones I wanted to focus on
-
+  skimr::skim(species, flashed) 
+#sp = "raadyr"
 sp = c("rev", "raadyr", "gaupe", "hjort", "grevling", "elg")
-# sp = c("rev", "raadyr")
-fit<-survfit(Surv(t.diff, event, type="right")~flashed + strata(validated_species), data=obs[obs$validated_species%in%sp & !obs$period%in%"Control",])
-ggsurvplot(fit, data = obs[obs$validated_species%in%sp & !obs$period%in%"Control",])
+obs_sp <- obs %>%  # making obs_sp for specific species, removing control group
+  filter(species %in% sp, !period == "Control")
 
-
-library(ViewPipeSteps)
-
-# Model formula displayer in RMarkdown!
-library(equatiomatic)
-
-# fit a basic multiple linear regression model
-model <- lm(price ~ carat + depth,
-            data = diamonds)
-
-extract_eq(model,
-           use_coefs = TRUE)
-
-#If the equation is long, you can display it on multiple lines by adding the argument wrap = TRUE:
-  
-  model <- lm(price ~ carat + x + y + z + depth,
-              data = diamonds)
-
-extract_eq(model,
-           use_coefs = TRUE,
-           wrap = TRUE)
+sp_mod0<-coxph(Surv(t.diff, event, type="right")~flashed 
+            + strata(species), data=obs_sp)
+summary(sp_mod0)
 
 
 
-# Fit (complexe) survival curves
 
-fit3 <- survfit( Surv(time, status) ~ sex + rx + adhere,
-                 data = colon )
+surv_diff <- survdiff(Surv(t.diff, event) ~ flashed + strata(loc), 
+                      data = obs_sp)
+surv_diff
+surv_diff_sp <- survdiff(Surv(t.diff, event) ~ species +
+                         flashed + strata(loc), data=obs_sp)
+surv_diff_sp
 
-# Visualize
-#++++++++++++++++++++++++++++++++++++
-ggsurv <- ggsurvplot(fit3, data = colon,
-                     fun = "cumhaz", conf.int = TRUE,
-                     risk.table = TRUE, risk.table.col="strata",
-                     ggtheme = theme_bw())
+
+fit<-survfit(Surv(t.diff, event, type="right") ~ flashed +  species,
+             data=obs_sp)
+ggsurvplot(fit, data = obs_sp,
+           ggtheme = theme_minimal(),
+           censor.shape = "|", censor.size = 3)
+
+
+
+# Visualizations ------------------------------------------------
+ggsurv <- ggsurvplot(fit, data = obs_sp,
+                     #fun = "cumhaz", conf.int = TRUE,
+                    # risk.table = T, risk.table.height = "none",
+                     risk.table.y.text.col = T,# colour risk table text annotations.
+                     risk.table.y.text = FALSE,# show bars instead of names in text annotations
+                     ggtheme = theme_bw(),  
+                     xlim = c(0,120)
+                     )
 
 # Faceting survival curves
-curv_facet <- ggsurv$plot + facet_grid(rx ~ adhere)
+curv_facet <- ggsurv$plot + facet_grid( ~ species) + theme(legend.position = "none")
 curv_facet
 
 # Faceting risk tables:
 # Generate risk table for each facet plot item
-ggsurv$table + facet_grid(rx ~ adhere, scales = "free")+
+ggsurv$table + facet_grid( ~ species, scales = "free") +
   theme(legend.position = "none")
 
 # Generate risk table for each facet columns
-tbl_facet <- ggsurv$table + facet_grid(.~ adhere, scales = "free")
+tbl_facet <- ggsurv$table + facet_grid(.~ species, scales = "free") 
+#+ theme(legend.position = element_blank())
 tbl_facet + theme(legend.position = "none")
 
 # Arrange faceted survival curves and risk tables
@@ -110,33 +84,16 @@ min_ncol <- min(ncol(g2), ncol(g3))
 g <- rbind(g2[, 1:min_ncol], g3[, 1:min_ncol], size="last")
 g$widths <- grid::unit.pmax(g2$widths, g3$widths)
 grid::grid.newpage()
-grid::grid.draw(g)
+grid::grid.draw(g) 
 
 
-## End(Not run)
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Example 3: CUSTOMIZED PVALUE
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Customized p-value
-fit1<- survfit(Surv(time, status) ~ sex, data = lung)
-
-ggsurvplot(fit1, data = lung, pval = TRUE, title = "Roe deer", ylab= "Capture probability")
-ggsurvplot(fit1, data = lung, pval = 0.03)
-ggsurvplot(fit1, data = lung, pval = "The hot p-value is: 0.031",surv.median.line = "hv")
-
-library(Rtool)
-
-# remotes::install_github("easystats/report") # finally worked after several days
-#  had to accept all updates (ie. type 1 at every prompt)
-library(report)
+# report --------------------------
+library(report) #not available for coxph >:^(
 library(tidyverse)
 dat <- mtcars %>%
   select(-vs, -am)
 test <- cor.test(dat$drat, dat$qsec)
 test
 report(test)
-
-remotes::install_github("easystats/easystats")
-
 
