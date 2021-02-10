10173a4d0458d06a4a0fcbd2109e0211671144c6
TorLinux <torgeirholval@gmail.com>
2021-02-10 16:49:21 +0100

    Tex med Holm metode, og citation-skript
    Lagd eit citations-skript slik at eg lettere kan hente ut referanser frå framtidige r-pakker, jobba med ein SessionInfo.tex-fil som genererest frå surv.rmd-fila. Skrevet om multiple hypothesis-testing (Holm 1979) i metodedel. Dette er rett etter 1. innsending av thesis til Neri.

---
+++ citations.R
@@ -0,0 +1,15 @@
+# Citing R packages
+options(citation.bibtex.max = 999) # forcing the printing of bibtex-entry by citation-function
+
+#already included tidyverse, lubridate, overlap, survival-package, survminer, RStudioTeam and RCoreTeam
+# 2021.02.09----------------------
+citation("base")
+citation("survival")
+citation("coxme")
+citation("lme4")
+citation("ggplot2") # could deserve a proper mention along the way.
+
+# 2021.02.
+# !!!
+
+knitr::write_bib()
